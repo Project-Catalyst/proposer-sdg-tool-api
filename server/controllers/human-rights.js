d380223b195ab635f5ae5a7cb40af99a15f8d68f
aim <plamen.krasimirov.petkov@gmail.com>
2022-04-30 14:34:23 +0300

    add human rights endpoint

--- server/controllers/human-rights.js
+++ server/controllers/human-rights.js
@@ -1,10 +1,27 @@
 const { StatusCodes } = require('http-status-codes')
+const { sequelize } = require('../db')
+const { Op } = require('sequelize')
 
 // Public Route
 // GET /api/v1/humanRights
 const getReq = async (req, res) => {
- 
-  res.status(StatusCodes.OK).json({ msg: "success" })
+  debugger
+  try {
+    const humanRights = await sequelize.models.HumanRight.findAll({
+      where: {
+        [Op.or]: [{
+          id: req.query?.ids ? req.query.ids : []
+        },{
+          title: req.query?.titles ? req.query.titles : []
+        }]
+      }
+    })
+    const humanRightsDV = humanRights?.map(x => x.dataValues)
+    res.status(StatusCodes.OK).json({ humanRightsDV, count: humanRightsDV.length })
+
+  } catch (e) {
+    res.status(StatusCodes.BAD_REQUEST).json({ error: e.toString() })
+  }
 }
 
 module.exports = {
